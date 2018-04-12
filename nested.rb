457a540024057bb1170a8fddea9c656dc6b54002
Laura Messa Travin <laura.travin@gmail.com>
2018-04-12 04:26:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,7 @@ def adding_matz
 		             :known_for => "Ruby",
 		             :languages => ["LISP", "C"]
 		                }
-		programmer_hash[:yukihiro_matsumoto].keys						
+		programmer_hash[:yukihiro_matsumoto].key					
 
 end
 
